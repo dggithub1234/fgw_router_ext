2bc4a9e4bc9fc16c588e8a18d87ad084a5a26237
Daniel G <154083145+dggithub1234@users.noreply.github.com>
2026-09-14 16:18:14 +0100

    Update device_tracker.py

--- custom_components/fgw_router/device_tracker.py
+++ custom_components/fgw_router/device_tracker.py
@@ -104,4 +104,4 @@ class FGWScannerEntity(ScannerEntity):
 
     async def async_added_to_hass(self) -> None:
         """Subscribe to coordinator updates."""
-        self.async_on_unload(self.coordinator.async_add_listener(self.async_write_ha_state))
+        self.async_on_remove(self.coordinator.async_add_listener(self.async_write_ha_state))
