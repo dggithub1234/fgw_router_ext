52a96e223a752c162236b6bfee07787c970dd283
Daniel G <154083145+dggithub1234@users.noreply.github.com>
2026-09-20 17:22:04 +0100

    Update __init__.py

--- custom_components/fgw_router/__init__.py
+++ custom_components/fgw_router/__init__.py
@@ -14,9 +14,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Altice / MEO FiberGateway from a config entry."""
     _LOGGER.debug("Setting up FiberGateway config entry: %s", entry.entry_id)
 
-    # Register the options update listener
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-
+    # Clean platform forwarding
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
@@ -25,11 +23,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a FiberGateway config entry when deleted or disabled."""
     _LOGGER.debug("Unloading FiberGateway config entry: %s", entry.entry_id)
     
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    return unload_ok
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry when options are updated."""
-    # This ensures that changing configurations updates cleanly across all layers
-    await hass.config_entries.async_reload(entry.entry_id)
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
