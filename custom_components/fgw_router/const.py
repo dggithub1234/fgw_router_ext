525bb6de3a6b5480faa26d545009b11f51f27c84
Daniel G <154083145+dggithub1234@users.noreply.github.com>
2026-09-20 17:02:39 +0100

    Update const.py

--- custom_components/fgw_router/const.py
+++ custom_components/fgw_router/const.py
@@ -4,3 +4,6 @@ DOMAIN = "fgw_router_ext"
 
 # Custom device tracker options key
 CONF_TRACK_NEW_DEVICES = "track_new_devices"
+
+# Add your local scan interval definition here to avoid Home Assistant Core deprecation errors
+CONF_SCAN_INTERVAL = "scan_interval"
